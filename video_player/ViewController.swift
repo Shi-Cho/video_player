afee3c8b53f31276794d1366f871c59f14e0dc49
CSC <csc@cs.ntust.edu.tw>
2023-02-27 10:05:27 +0800

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Shi-Cho Cha on 2023/2/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
